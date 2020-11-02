e32786b3d07d934cea05a44ea2fd6085be89a65b
ManuDeveloper <manuepeva@gmail.com>
2020-11-02 12:39:59 -0500

    Adding layout to pages

--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../components/header'
+import Layout from '../components/layout'
 
 const Nosotros = () => {
     return (
-        <div>
+        <Layout>
             <h2>From Nosotros</h2>
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/propiedades.js
+++ src/pages/propiedades.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 const Propiedades = () => {
     return (
-        <div>
+        <Layout>
             <h1>Propiedades</h1>
-        </div>
+        </Layout>
     )
 }
 
